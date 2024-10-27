082968d460eda3fd9f6559bc5cb1871ea85c011e
Vishal Chandran <vishalchandran@gmail.com>
2024-10-27 22:13:55 +0530

    Added button actions

--- FaceTrackDemo/Views/HomeViewController.swift
+++ FaceTrackDemo/Views/HomeViewController.swift
@@ -9,6 +9,24 @@ import UIKit
 
 class HomeViewController: BaseViewController {
 
+    // MARK: - IBActions
+
+    @IBAction func visionKitButtonTapped(_ sender: UIButton) {
+        guard let visionKitVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: VisionKitViewController.typeName) as? VisionKitViewController else {
+            return
+        }
+
+        show(visionKitVC, sender: self)
+    }
+
+    @IBAction func arKitButtonTapped(_ sender: UIButton) {
+        guard let arKitVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: ARKitViewController.typeName) as? ARKitViewController else {
+            return
+        }
+
+        show(arKitVC, sender: self)
+    }
+
     // MARK: - View Life Cycle Methods
 
     override func viewDidLoad() {
